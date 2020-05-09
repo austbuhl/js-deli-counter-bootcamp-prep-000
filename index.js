005b0bc649a879547f12e70430efec5b9bdb7295
Austin Buhler <austinbuhler@gmail.com>
2020-05-09 14:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 function takeANumber(line, name) {
-  for (let i = 1; i <= line.length; i++) {
-    console.log(`Welcome, ${name}. You are number ${i} in the line.`);
-  }
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
