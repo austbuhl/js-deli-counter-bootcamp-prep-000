7960e311eaece043594bae3f7fbf61417b7bcfd8
Austin Buhler <austinbuhler@gmail.com>
2020-05-09 14:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(line) {
     return "The line is currently empty."
   } else {
     for (let i = 0; i < line.length; i++) {
-      array.push(`[i+1]. line[i]`)
+      array.push(`i+1. line[i]`)
     }
   }
   return "The line is currenty:" + array
