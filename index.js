d0c4d30d692d1233e21ae10fc8a1b4ea27a92c43
Austin Buhler <austinbuhler@gmail.com>
2020-05-09 14:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving line[0].`;
+    line.shift();
+  }
+}
